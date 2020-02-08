461e8fa310b444a6b81fed851d3b84bb353160fd
Nikos <dynarithm@gmail.com>
2020-02-08 16:43:15 +0200

    Refactor GraphOfDocs.py to support command line arguments
    Most parameters of the application can now be defined, before the script is executed.

--- GraphOfDocs.py
+++ GraphOfDocs.py
@@ -4,8 +4,10 @@ from neo4j import ServiceUnavailable
 from GraphOfDocs.neo4j_wrapper import Neo4jDatabase
 from GraphOfDocs.utils import generate_words, read_dataset, clear_screen
 from GraphOfDocs.create import *
+from GraphOfDocs.parse_args import parser
 
-def graphofdocs(create = False):
+def graphofdocs(create, initialize, dirpath, window_size, 
+    extend_window, remove_stopwords, lemmatize, stem):
     uri = 'bolt://localhost:7687'
     username = 'neo4j'
     password = '123'
@@ -31,7 +33,7 @@ def graphofdocs(create = False):
         database.execute('CREATE CONSTRAINT ON (word:Word) ASSERT word.key IS UNIQUE', 'w')
 
         # Read text from files, which becomes a string in a list called dataset.
-        dataset = read_dataset('C:\\Users\\USER\\source\\repos\\GraphOfDocs\\GraphOfDocs\\GraphOfDocs\\jira_issues\\')
+        dataset = read_dataset(dirpath)
         count = 1
         total_count = len(dataset)
         # Iterate all file records of the dataset.
@@ -39,18 +41,15 @@ def graphofdocs(create = False):
             # Print the number of the currently processed file.
             print('Processing ' + str(count) + ' out of ' + str(total_count) + ' datasets...' )
             # Generate the terms from the text of each file.
-            words = generate_words(file)
+            words = generate_words(file, extend_window, remove_stopwords, lemmatize, stem)
             # Create the graph of words in the database.
-            value = create_graph_of_words(words, database, filename)
+            value = create_graph_of_words(words, database, filename, window_size)
             if value is not None:
                 skipped.append(value)
             # Update the progress counter.
             count = count + 1
             # Clear the screen to output the update the progress counter.
             clear_screen(current_system)
-        run_initial_algorithms(database)
-        create_similarity_graph(database)
-        create_clustering_tags(database)
         # Count all skipped files and write their filenames in skipped.log
         skip_count = len(skipped)
         print('Created '+ str(total_count - skip_count) +', skipped '+ str(skip_count) +' files.')
@@ -58,7 +57,41 @@ def graphofdocs(create = False):
         with open('skipped.log', 'w') as log:  
             for item in skipped:
                 log.write(item + '\n')
+
+    if initialize:
+        # Run initialization functions.
+        run_initial_algorithms(database)
+        create_similarity_graph(database)
+        create_clustering_tags(database)
+
     database.close()
     return
 
-if __name__ == "__main__": graphofdocs()
+if __name__ == '__main__': 
+    # If only one argument is specified,
+    # Then it's the script name.
+    # Print help for using the script and exit.
+    if len(sys.argv) == 1:
+        parser.print_help()
+        parser.exit()
+
+    # Parse all arguments from terminal.
+    args = parser.parse_args()
+
+    # If create flag is set but no dirpath is specified, print error.
+    if args.create and args.dirpath is None:
+        parser.error('Please set the dirpath flag and specify a valid filepath!')
+    # Else if create flag is specified along with a valid dirpath.
+    elif args.create:
+        print(args)
+        # Run the graphofdocs function with create and initialize set to True.
+        # The first argument (0th index) after the dirpath flag is the actual directory path. 
+        graphofdocs(True, True, args.dirpath[0], args.window_size,
+                   args.extend_window, args.remove_stopwords, args.lemmatize, args.stem)
+    # Else if reinitialize flag is specified, unset the create flag.
+    elif args.reinitialize:
+        print(args)
+        # Run the graphofdocs function with create set to False and initialize set to True.
+        # We also set the directory path to None, since its not needed.
+        graphofdocs(False, True, None, args.window_size,
+                   args.extend_window, args.remove_stopwords, args.lemmatize, args.stem)
