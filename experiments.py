77abbf3c3bdedb47f17d9c430752c6982a02407e
Nikos <dynarithm@gmail.com>
2020-03-19 18:34:50 +0200

    Suppress warnings in experiments.py, Remove redundant run_experiments.sh

--- experiments.py
+++ experiments.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.warn = lambda *args, **kwards: None # Supress warnings.
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
--- run_experiments.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python experiments.py > experimental_results/20newsgroups/experimental_results.txt 2> experimental_results/20newsgroups/experimental_results_errors.txt
